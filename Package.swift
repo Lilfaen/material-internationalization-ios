091636408334abc6d068684fd64fcccfbf49de7c
Clemens Beck <clemensbeck@icloud.com>
2023-04-22 20:53:47 +0200

    Fix naming mistake

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MaterialComponents",
+    name: "MDFInternationalization",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
